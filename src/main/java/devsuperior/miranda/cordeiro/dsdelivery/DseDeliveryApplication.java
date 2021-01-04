94fd9af66e20243d00b69b5a80cb7429919ff97b
Ewerton <sonardevplus@gmail.com>
2021-01-04 12:48:02 -0300

    project created

---
+++ src/main/java/devsuperior/miranda/cordeiro/dsdelivery/DseDeliveryApplication.java
@@ -0,0 +1,13 @@
+package devsuperior.miranda.cordeiro.dsdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DseDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DseDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devsuperior/miranda/cordeiro/dsdelivery/DseDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package devsuperior.miranda.cordeiro.dsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DseDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
